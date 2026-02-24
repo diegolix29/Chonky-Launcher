3d043800e203207cab2d879b917ef963198362d0
GitHub Action <action@github.com>
2026-02-24 00:50:46 +0000

    Bump version to 1.0.7

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     
     app.setApplicationName("ChonkyStation Launcher");
-    app.setApplicationVersion("1.0.6");
+    app.setApplicationVersion("1.0.7");
     app.setOrganizationName("ChonkyLauncher");
     
     ChonkyLauncher launcher;
